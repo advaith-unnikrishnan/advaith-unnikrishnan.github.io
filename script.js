b73b36cc1429d593c960d03dfcbbd8cfe6f0a932
ADVAITH U <45172876+advaith-unnikrishnan@users.noreply.github.com>
2021-01-20 23:33:53 +0530

    wip: remove unused files

--- script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.querySelector(".nav-buttons")
-    if (x.className === "links") {
-      x.className += "responsive";
-    } else {
-      x.className = "nav-buttons";
-    }
-  }
